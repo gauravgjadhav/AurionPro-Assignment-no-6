8c56595cf132be96e8f54a16ac19d42eaabd0db8
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-06-18 16:48:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string Strings = "Sachin,AurionPro,Mumbai";
+        string[] stringList = Strings.Split(",");
+        Console.WriteLine("Devoloper name is: " + stringList[0]);
+        Console.WriteLine("Company name is: " + stringList[1]);
+        Console.WriteLine("Locaiton is : " + stringList[2]);
+    }
+}
